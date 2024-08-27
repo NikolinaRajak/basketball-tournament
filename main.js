c97ce2bf08652049e65dc702bd257e9b855659df
NikolinaRajak <nikolina.rajak2019@gmail.com>
2024-08-27 18:26:13 +0200

    simulate group matches implementation started

---
+++ main.js
@@ -0,0 +1,79 @@
+const data = require('./groups.json');
+//console.log(data);
+
+const a = data.A;
+console.log(a);
+
+function simulateGroup(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        const first_team = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            const second_team = array[j];
+            let { winProbability1, winProbability2 } = calculateProbability(first_team.FIBARanking, second_team.FIBARanking);
+            let { outcome1, outcome2 } = simulateGroupMatch(winProbability1, winProbability2);
+                console.log(first_team.Team + "(" + winProbability1 + ", " + outcome1 + ")" + " vs " + second_team.Team + "(" + winProbability2 + ", " + outcome2 + ")")
+        }
+    }
+}
+
+function generateResult(tie) {
+    
+
+
+}
+
+function simulateGroupMatch(prob1, prob2) {
+
+    let random1 = Math.random();
+    let random2 = Math.random();
+    let outcome1, outcome2;
+
+    if (random1 <= prob1) {
+        outcome1 = 'win';
+    } else {
+        outcome1 = 'loss';
+    }
+
+    if (random2 <= prob2) {
+        outcome2 = 'win';
+    } else {
+        outcome2 = 'loss';
+    }
+
+    return { outcome1, outcome2 };
+}
+
+function calculateProbability(a, b) {
+
+    let rankDifference = Math.abs(a - b);
+
+    // Calculating the win probability (for the better team)
+    let winProbability;
+
+    if (rankDifference <= 5) {
+        winProbability = 0.55;
+    } else if (rankDifference <= 10) {
+        winProbability = 0.65;
+    } else {
+        winProbability = 0.75;
+    }
+
+    // Assigning the win probabilities to 
+    // two variables (for the better and worse team)
+    let winProbability1, winProbability2;
+
+    if (a < b) {
+        winProbability1 = winProbability;
+        winProbability2 = 1 - winProbability
+    } else {
+        winProbability2 = winProbability;
+        winProbability1 = 1 - winProbability
+    }
+
+    return { winProbability1, winProbability2 }
+
+}
+
+
+
+simulateGroup(a);
