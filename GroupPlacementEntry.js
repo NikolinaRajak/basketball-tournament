3abd0789adcf358e883b05b322dfbbf544868c5f
NikolinaRajak <nikolina.rajak2019@gmail.com>
2024-08-29 21:35:09 +0200

    created group table list and class for storaging info

---
+++ GroupPlacementEntry.js
@@ -0,0 +1,30 @@
+class GroupPlacementEntry {
+
+    constructor(name) {
+        this.name = name;
+        this.wins = 0;
+        this.losses = 0;
+        this.points = 0;
+        this.scoredBaskets = 0;
+        this.receivedBaskets = 0;
+        this.basketDifference = 0;
+    }
+
+    addGame(win, scoredBaskets, receivedBaskets) {
+        if (win) {
+            this.wins += 1;
+            this.points += 2;
+        } else {
+            this.losses += 1;
+            this.points += 1;
+        }
+
+        this.scoredBaskets += scoredBaskets;
+        this.receivedBaskets += receivedBaskets;
+
+        this.basketDifference = this.scoredBaskets - this.receivedBaskets;
+
+    }
+
+}
+module.exports = GroupPlacementEntry;
--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 const data = require('./groups.json');
-//console.log(data);
+const GroupPlacementEntry = require('./GroupPlacementEntry.js')
 
 const a = data.A;
 console.log(a);
 
 function simulateGroup(array) {
-
+    let groupTable = [];
     for (let i = 0; i < array.length - 1; i++) {
         const first_team = array[i];
         for (let j = i + 1; j < array.length; j++) {
             const second_team = array[j];
-            let  winProbability = calculateProbability(first_team.FIBARanking, second_team.FIBARanking);
+            let winProbability = calculateProbability(first_team.FIBARanking, second_team.FIBARanking);
             let { outcome1, outcome2 } = simulateGroupMatch(winProbability);
             let { result1, result2 } = generateResult(outcome1);
-            console.log(first_team.Team + "(" + winProbability + ", " + outcome1 + ", " + result1 + ")" + " vs " + second_team.Team + "(" + (1- winProbability) + ", " + outcome2 + ", " + result2 + ")")
+            console.log(first_team.Team + "(" + winProbability + ", " + outcome1 + ", " + result1 + ")" + " vs " + second_team.Team + "(" + (1 - winProbability) + ", " + outcome2 + ", " + result2 + ")")
+            // Updating or instantiating the first team
+            let team1Index = groupTable.findIndex(team => team.name === first_team.Team);
+            if (team1Index !== -1) {
+                groupTable[team1Index].addGame(outcome1 == 'win', result1, result2);
+            } else {
+                let team1 = new GroupPlacementEntry(first_team.Team);
+                team1.addGame(outcome1 == 'win', result1, result2);
+                groupTable.push(team1);
+            }
+            // Updating or instantiating the second team
+            let team2Index = groupTable.findIndex(team => team.name === second_team.Team);
+            if (team2Index !== -1) {
+                groupTable[team2Index].addGame(outcome2 == 'win', result2, result1);
+            } else {
+                let team2 = new GroupPlacementEntry(second_team.Team);
+                team2.addGame(outcome2 == 'win', result2, result1);
+                groupTable.push(team2);
+            }
         }
     }
+    console.log(groupTable);
 }
 
 function generateResult(outcome1) {
@@ -23,7 +42,7 @@ function generateResult(outcome1) {
     let genRes2 = genRes1 - Math.round(Math.random() * 21) - 1;
 
     let result1, result2;
-    
+
     if (outcome1 == 'win') {
         result1 = genRes1;
         result2 = genRes2;
@@ -33,7 +52,7 @@ function generateResult(outcome1) {
     }
 
     return { result1, result2 }
-    
+
 }
 
 function simulateGroupMatch(winProbability) {
